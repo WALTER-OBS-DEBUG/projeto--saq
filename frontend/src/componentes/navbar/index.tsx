72f3e6a71970a776e121d87dc6d62582a66e3d21
WALTER-OBS-DEBUG <jonasdowalter@gmail.com>
2022-01-20 01:18:05 -0400

    Mudando o local das imagens

--- frontend/src/componentes/navbar/index.tsx
+++ frontend/src/componentes/navbar/index.tsx
@@ -7,11 +7,11 @@ const NavBar = () => {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                   <h2 className="text-primary">Sistema de Aluguel de Quadras Esportivas
+                   <h2 className="text-primary">Sistema de Aluguel de Quadras Esportivas</h2>
                     <img  src={BoladeFutebol} alt="SAQ" width ="50" /> 
                     <img  src={BoladeBasquete} alt="SAQ" width ="50" />
                     <img  src={BolaDeVolei} alt="SAQ" width ="60" />
-                   </h2>
+                   
                 </nav>
             </div>
       </div>
